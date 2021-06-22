6a4966edcd3c32f8e28a996ec5776545ac0eb9cb
ashley <ashley@dunamu.com>
2021-06-22 23:23:41 +0900

    initial commit

---
+++ src/main/kotlin/game/cardy/cardy/CardyApplication.kt
@@ -0,0 +1,11 @@
+package game.cardy.cardy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CardyApplication
+
+fun main(args: Array<String>) {
+    runApplication<CardyApplication>(*args)
+}
---
+++ src/test/kotlin/game/cardy/cardy/CardyApplicationTests.kt
@@ -0,0 +1,13 @@
+package game.cardy.cardy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CardyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
